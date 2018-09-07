4411f6e6b24d017ba890c76afd4369ac84d4c527
Garrett Thompson <garrett6346@gmail.com>
2018-09-07 10:55:17 -0400

    Open sourcing this helper

---
+++ get.js
@@ -0,0 +1,16 @@
+/**
+ * Documentation
+ * @param {{}} obj
+ * @param {string[]} path
+ * @param {{string: boolean}} options "string" determines the return value
+ */
+const get = (obj, path, options) => {
+  const { string } = options || { string: false };
+
+  return path.reduce(
+    (o, key) => (o && o[key] ? o[key] : string ? "" : null),
+    obj
+  );
+};
+
+module.exports = get;
